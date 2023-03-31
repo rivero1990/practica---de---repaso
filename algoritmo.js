9eb16e642cac1c6be5f923dffa7b6a28b7871558
Anibal Rivero <riveroleonel@gmail.com>
2023-03-31 13:28:29 -0300

    practica de repaso

---
+++ algoritmo.js
@@ -0,0 +1,45 @@
+document.querySelector("body").innerHTML = `
+h1>Ultimos ganadores de la copa libertadores</h1>
+    <p>Periodo entre 2013 hasta 2022</p>
+    
+    <ul>
+        <li>Atetlico mineiro</li>
+        <li>San Lorenzo</li>
+        <li>River Plate</li>
+        <li>Atletico Ncacional</li>
+        <li>Gremio poto alegre</li>
+        <li>River Plate</li>
+        <li>Flamengo</li>
+        <li>Palmeiras</li>
+        <li>Palmeiras</li>
+        <li>Flamnego</li>
+    </ul>
+
+    <h1>Video de la final de libertadores 2018</h1>
+
+    <details>
+        <summary>final de madrid</summary>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/F-A_ujDnMo4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </details>
+
+    <h1>Fotos de la entrega de la copa</h1>
+
+    <details>
+        <summary>enterga de la copa libertadores</summary>
+        <img src="https://th.bing.com/th/id/OIP.ZmwzcNyikZO89zkw03mO5AHaE8?w=269&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="foto de river">
+        <img src="https://th.bing.com/th/id/OIP.oC12rso46DXbhOtq6gNHrAHaEK?w=286&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="">
+    </details>
+
+    <h1>fixture fase de grupos de la libertadores para river plate</h1>
+
+    <ol>
+        <li>martes 4/4 fecha 1 the strongest vs river plate</li>
+        <li>miercoles 19/4 fecha 2 river plate vs sporting cristal</li>
+        <li>martes 2/5 fecha 3 fluminense vs river plate</li>
+        <li>fecha 4 jueves 25/5 sporting cristal vs river plate</li>
+        <li>miercoles 7/6 fecha 5 river plate vs fluminense</li>
+        <li>martes 27/6 fecha 6 river plate vs the strongest</li>
+    </ol>
+
+   <a href="https://www.ole.com.ar/river-plate/cuantas-copas-libertadores-gano-river_0_Q6W5s0EkL.html">enlace hacia la pagina de ole de cuantas libertadores tiene river plate</a><
+`;
